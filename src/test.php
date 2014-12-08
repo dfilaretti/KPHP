76722757e2c8c2f09806beb416887298db340abc
dfilaretti <dfilaretti@gmail.com>
2014-12-08 10:18:31 +0000

    changing folder structure

--- src/test.php
+++ src/test.php
@@ -1,19 +1,3 @@
 <?php
-
-$x = 1;
-$y = 4;
-echo $x + $y;
-
-
-class A {
-    public $test = 1;
-};
-
-class B extends A {};
-
-$obj = new A();
-
-if ($obj instanceof A) {
-   echo 'A';
-}
+    echo "Hello world\n";
 ?>
