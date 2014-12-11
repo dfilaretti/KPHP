e75af42d19c0ce2785d3ac31598e4955e88889a5
dfilaretti <dfilaretti@gmail.com>
2014-12-11 16:18:13 +0000

    working on the static analyser

--- src/test.php
+++ src/test.php
@@ -1,10 +1,21 @@
 <?php
 
+$x = user_input();
+
+/*
     class Foo {
         public $x = 1;
     }
 
     $obj = new Foo();
+*/
 
-
+/*
+if (x) {
+    echo "y";
+}
+else {
+    echo "n";
+}
+*/
 ?>
