dd6e5b30fac6c66a3055c26a1bf91f4f9a0a925a
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-01-16 09:39:48 +0000

    stable commit

--- src/test.php
+++ src/test.php
@@ -1,14 +1,58 @@
 <?php
 
+$x = 0;
+
+while(true) {
+	echo "$x\n";
+	$x = $x + 1;
+}
+
+/*
+$x = 1;
+
+while ($x < 20) {
+	echo "x\n";
+	$x = $x + 1;
+}
+*/
+
+
+// ----- 2
+
+/*
 $y = user_input();
 
 if ($y) {
-    $q = 1;
-//    $z = array();
+    $q = -20;
+    //$z = array(array(2));
 }
 else {
-    $z = 3;
-//    $z = array();
+    $q = -10;
+    //$z = array(array(0));
 }
+*/
+
+
+// ----- 1
+
+/* 
+function foo($x) {
+	$y = user_input();
+
+
+	if ($y) {
+    		$q = -20;
+    		//$z = array(array(2));
+	}
+	else {
+    		$q = -10;
+    		//$z = array(array(0));
+	}
+}
+
+foo(4);
+*/
+
+
 
 ?>
