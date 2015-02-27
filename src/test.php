d2681bb3200fdb16e374feea34c35507d2cd4bf1
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-02-27 16:29:45 +0000

    stable

--- src/test.php
+++ src/test.php
@@ -1,22 +1,3 @@
---TEST--
-Dynamic access of static members
---FILE--
 <?php
-class A {
-    public    static $b = 'foo';
-}
-
-$classname       =  'A';
-$wrongClassname  =  'B';
-
-
-/*
-echo $classname::$b."\n";
-echo $wrongClassname::$b."\n";
-*/
+	echo(10/3);
 ?>
-===DONE===
---EXPECTF--
-foo
-
-Fatal error: Class 'B' not found in %s041.php on line %d
