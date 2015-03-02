ec8ad6aa33000f9fc90a785e826ea90e640dfed3
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-03-02 14:46:12 +0000

    asd

--- src/test.php
+++ src/test.php
@@ -1,54 +1,13 @@
 <?php
-// This causes issues with refcounting
-
-if (2 == 4) {
 	$x = 1;
-	$y = 88;
-}
-else {
-	$x = -1;
-	$y = &$x;
-}
+	$y =& $x;
+	//unset($x);
 
-/*
-$x = 123;
-function &foo() {
-	global $x;
-	$y = 1;
-	if (2 == 4)  {
-		return $x;
-	} 
-	else {
-		return $y;
-	}
-}
-$result = &foo();
-*/
 
-/*
-$val = 123;
-function &foo(&$x) {
-	$y = 1;
-	if (2 == 4)  {
-		return $x;
-	} 
-	else {
-		return $y;
+	function foo($x) {
+		return $x + 1;
 	}
-}
-$result = &foo($val);
-*/
 
-/*
-function &foo() {
-	$x = -1;
-	$y = 1;
-	if (2 == 4)  {
-		return $x;
-	} 
-	else {
-		return $y;
-	}
-}
-$result = &foo();
-*/
+	$z = foo(123);
+
+?>
