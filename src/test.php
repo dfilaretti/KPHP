e81781a8b782d56be9baf9a4178cdbb76a46ee6a
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-03-25 14:39:05 +0000

    fixed merge

--- src/test.php
+++ src/test.php
@@ -1,13 +1,14 @@
 <?php
 
+// test passing parameters by ref
+// this was challenging 
+
 $v = 1;
-
-
 function foo(&$x) {
+	// conditional does nothing but forces state merge
 	if (2 == 1) {1;} else {2;};
 	$x = 0;
 }
-
 foo($v);
 
 
