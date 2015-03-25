22c14c1bb73142ec17d3ed94a33e39fa1c4bc912
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-03-25 07:53:07 +0000

    asd

---
+++ src/tests/analysis-bugs/bug-while.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+/* Iterations are done. The problem is at the end! */
+
+
+
+
+$x = 0;
+
+
+while ($x != 2) {
+	echo $x . "\n";
+	$x = $x + 1;
+}
+
+
+
+
+echo "hello";
+
+
+/*
+$x = 0;
+
+while (true) {
+	echo $x . "\n";
+	$x = $x + 0;
+}
+*/
+
+
+// ----- 2
+
+/*
+$y = user_input();
+
+if ($y) {
+    $q = -20;
+    //$z = array(array(2));
+}
+else {
+    $q = -10;
+    //$z = array(array(0));
+}
+*/
+
+
+// ----- 1
+
+/* 
+function foo($x) {
+	$y = user_input();
+
+
+	if ($y) {
+    		$q = -20;
+    		//$z = array(array(2));
+	}
+	else {
+    		$q = -10;
+    		//$z = array(array(0));
+	}
+}
+
+foo(4);
+*/
+
+
+
+?>
