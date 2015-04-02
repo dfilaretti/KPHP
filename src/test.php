d3730d3cf8ec5ff9c4e8786e5f6b79219b2ad659
Daniele Filaretti <d.filaretti11@imperial.ac.uk>
2015-04-02 16:15:09 +0100

    a few more fixes on memory merge. Some progress but still some (minor) issues

--- src/test.php
+++ src/test.php
@@ -1,11 +1,18 @@
 <?php
 
+// expecting MUST aliasing but getting MAY
+
+	// may aliasing {x, y, z}
+	$x = 0;
 	if (2 ==3) {
-		$x = array("foo" => 0, "bar" => 2);	
-		$y =& $x;		
+		$y =& $x;
+		$z = 1;
+	} else
+	{
+		$y = 2;
+		$z =& $y; 
 	}
-	else {
-		$x = array("foo" => 0, "bar" => -2);	
-		$y = "hello";
-	}
+	echo "DONE";
+
+
 ?>
