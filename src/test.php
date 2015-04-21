8a07f01d979dcbf9f8952de910d4d613bd0d7e5c
Daniele Filaretti <dfilaretti@Danieles-MacBook-Pro.local>
2015-04-21 11:34:20 +0100

    making sure I committed everything

--- src/test.php
+++ src/test.php
@@ -1,28 +1,13 @@
-<?php
+--TEST--
+Testing eval function inside user-defined function
+--FILE--
+<?php 
+function F ($a) { 
+	eval($a);
+}
 
-
-	// may aliasing {x, y, z}
-	$x = array(1,2,3);
-	if (2 == 3) {
-		$y =& $x;
-	} else
-	{
-		$y =& $x;
-	}
-	echo "DONE";
-
-/*
-	// may aliasing {x, y, z}
-	$x = 0;
-	if (2 ==3) {
-		$y =& $x;
-		$z = 1;
-	} else
-	{
-		$y = 2;
-		$z =& $y; 
-	}
-	echo "DONE";
-*/
-
+error_reporting(0);
+F("echo \"Hello\";");
 ?>
+--EXPECT--
+Hello
