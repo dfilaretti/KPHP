76b71ecab4b952886dcff6582e5f0ce352bddf10
Daniele Filaretti <dfilaretti@dyn1212-115.wlan.ic.ac.uk>
2015-05-20 14:40:31 +0100

    minor cleanup stuff

--- src/test.php
+++ src/test.php
@@ -4,15 +4,9 @@
 
 	// $x[Pos] and $x[Neg] are disjoint
 
-	$y = $x[1 - 1]; // $y = $x[Top]
-
-	// $x[Top] has been created as a side-effect of 
-	// reading it. This caused the aliasing of the existing
-	// keys Top and Neg. 
-
 	$x[2] =& $y;
 
-	// BUG! The reference assignment causes the "intersecting keys"
-	// to be aliased as well, that is {Pos, Top}. 
-	// But unfortunately, Neg doesn't get updated, while it should! 	
+	// Since the keys Pos and Neg are disjointed, the reference
+	// assignment only affected Pos. 
+	// This coincides with the expected, pre-analyis behaviour.  
 ?>
