7673c275fbc27a8e5eae36394acde38c3340fb5f
Daniele Filaretti <dfilaretti@dyn1208-134.wlan.ic.ac.uk>
2015-05-21 10:38:26 +0100

    added missing object case in LgetRef

--- src/test.php
+++ src/test.php
@@ -1,30 +1,15 @@
 <?php 
-	$x = "hello world";
-	$y =& $x;
 
-	// standard behaviour 'till now...
-
-	$v[0] =& $y;
-
-	// standard behaviour too... 
-
-	$t[1] =& $v[0];
-
-	// this also looks ok 
-
-	$v[1 - 2] = 123;
-
-	// now this "destroys" the array a little bit. 
-	// But looks ok
-
-	// MINOR BUG: should also update refcound! 
+	class A {
+		public $x = 123;
+	}
 
 
+	$o = new A();
+
+	$o -> foo = -1;
 
 
-
-
-
-
+	//var_dump($o);
 
 ?>
