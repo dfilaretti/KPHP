14e5d58bf8840cf66d2b945d94c2b58c02346b35
Daniele Filaretti <dfilaretti@dyn1209-77.wlan.ic.ac.uk>
2015-05-22 11:32:08 +0100

    extended new array semantics to objects

--- src/test.php
+++ src/test.php
@@ -5,11 +5,22 @@
 	}
 
 
+	// we need some "StringTop"
+	if (2 == 1) {
+		$k = "hello";
+	}
+	else {
+		$k = "world";
+	}
+
+
 	$o = new A();
-
 	$o -> foo = -1;
+	$o -> foo = 0;
 
-
+	$k1 = "foo";	
+	
+	$o -> $k =& $k1;
+	
 	//var_dump($o);
-
 ?>
