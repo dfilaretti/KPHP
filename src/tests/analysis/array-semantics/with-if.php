f30bd5cb1cd6c2ad07883d3dffb92502c639520c
Daniele Filaretti <dfilaretti@dyn1213-35.wlan.ic.ac.uk>
2015-05-27 13:58:25 +0100

    added new tests for analysis

--- src/tests/analysis/array-semantics/with-if.php
+++ src/tests/analysis/array-semantics/with-if.php
@@ -4,16 +4,12 @@
 	$x[-1] = 3;	// $x[Neg]  = Pos
 
 	if (2 == 2) {
-		// Note: it will get stuck also with 
-		// $x[1] = ...
 		$x[2 - 2] = 3;		// $x[Top] = Pos
 	}
 	else {
 		$x[1] = 2;		// $x[Pos] = Pos
 	}
 
-	// BUG: this gets completely stuck in SIGNS domain!
-	// NOTE: works ok in concrete...	
 
 
 ?>
