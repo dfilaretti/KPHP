d29c879a423570de978e139fa56ff0f97a583b4a
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-06-01 12:46:13 +0100

    Fix a bug with merge(DONE, BREAK)

--- src/test.php
+++ src/test.php
@@ -1,22 +1,29 @@
 <?php 
-$x="base";
-
-function F()
-{
-global $x;
-$x="guard";
-return true;
-}
 
 
-if (F()) {
-echo "true";
-echo $x;
-}
-else {
-echo "false"; 
-echo $x;
-}
+$i=1;
+$j=1;
 
-	
+switch($i) {
+    case 0:
+      echo "In branch 0\n";
+      break;
+    case 1:
+      echo "In branch 1\n";
+      break;
+    case 2:
+      echo "In branch 2\n";
+      break;
+    case 3:
+      echo "In branch 3\n";
+      break 2;
+    case 4:
+      echo "In branch 4\n";
+      break;
+    default:
+      echo "In default\n";
+      break;
+  }
+
+echo $j;  
 ?>
