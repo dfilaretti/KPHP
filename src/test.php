399e6a2798e569ae38aeb96c51ad7d8178c2ad8c
Daniele Filaretti <dfilaretti@dyn1212-136.wlan.ic.ac.uk>
2015-06-15 15:51:25 +0100

    abstracting reference counters.

--- src/test.php
+++ src/test.php
@@ -1,26 +1,20 @@
 <?php
-	// expecting MUST aliasing but getting MAY
 
+$a1 = array ("foo" => 1 ,"bar" => 2); $a2 = array ("foo" => 3 ,"bar" => 4);
+$ref = &$a1; // aliasing on $a1
+foreach ($a1 as $v){ echo "$v,"; if ($v === $a["foo"]) $a1=$a2; };
 
 /*
-$x = 1;
-if (2 == 2) {
-	$y = 1;
-}
-else {
-	$y = -1;
-}
+$x = array("foo" => 1, "bar" => 2);
+$z =& $x["foo"];
+$q =& $z;
 
+unset($q);
+unset($q);
+unset($q);
+unset($q);
+
+
+$y = $x;
 */
-
-
-	$x = array("foo" => 1, "bar" => 2);
-
-	if (2 == 2) {
-		$y =& $x["foo"];
-	}
-	else {
-		$y =& $x["foo"];
-	}	
-
 ?>
