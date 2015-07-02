969ab40a6c9928298ce9bf267836e12c67405668
Daniele Filaretti <dfilaretti@dyn1211-83.wlan.ic.ac.uk>
2015-07-02 09:22:37 +0100

    commit just to make sure everything is up to date

--- src/test.php
+++ src/test.php
@@ -1,60 +1,20 @@
+--TEST--
+Testing recursive function
+--FILE--
 <?php
 
-
-$x = array("foo" => 1);
-$y =& $x;
-$z =& $y;
-//$p =& $z;
-
-unset($y);
-//unset($z);
-//unset($p);
-unset($x);
-
-/*
-class A{
-	public $foo = 1;
+function Test($a)
+{
+   echo "$a ";
+   $a++;
+   if($a<10): Test($a); endif;
 }
 
-$o = new A;
-
-
-if (2 == 2) {
-	$x =& $o;
-	//$o -> foo = -1;
-}
-else {
-	$x =& $o;
-	//$o -> bar = 0;
-}
-*/
-
-/*
-
-if (2 == 2) {
-	$x -> foo = 1;
-	//$y =& $x;
-}
-else {
-	$x -> foo = 2;
-	//$y =& $x;
-}
-
-*/
-
-
-/*
-$y = 1;
-
-if (2 == 2) {
-	$x =& $y;	
-}
-else {
-	$x =& $y;
-	$z =& $x;
-}
-*/
-
-
+Test(1);
 
 ?>
+--EXPECT--
+1 2 3 4 5 6 7 8 9
+
+
+
