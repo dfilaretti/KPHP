0030720098a92ec3d2a528c892fa77e96e69d00d
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-07-20 15:37:10 +0100

    More merging changes

--- src/test.php
+++ src/test.php
@@ -1,40 +1,20 @@
-
-<?php 
-
-$i="abc";
-
-//for ($j=0; $j<10; $j++) {
-switch (1) {
-  case 1:
-  	echo "In branch 1\n";
-	
-  	switch ($i) {
-  		case "ab":
-  			echo "This doesn't work... :(\n";
-  			break;
-  		default:
-  			echo "Inner default...\n";
-  	}
-	
-  	/*for ($blah=0; $blah<2; $blah++) {
-  	  if ($blah==1) {
-  	    echo "blah=$blah\n";
-  	  }
-  	}*/
-  	break;
-	
-  case 2:
-  	echo "In branch 2\n";
-  	break;
-  case 3:
-  	echo "In branch \$i\n";
-  	break;
-  case 4:
-  	echo "In branch 4\n";
-  	break;
-  default:
-  	echo "Hi, I'm default\n";
-  	break;
- }
-//}
+<?php
+	function foo() {
+		echo "foo";
+		return 1;
+	}
+	function bar() {
+		echo "bar";
+		return 2;
+	}
+	function baz() {
+		echo "baz";
+		return 3;
+	}
+	if (2 == 2) 
+		$f = "foo";
+	else
+		$f = "bar";
+	$result = $f(1, 2, 3, 4, 5);
+	echo "hey";
 ?>
